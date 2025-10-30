4f5eb25bccd95b8610003a24eeda11477db78334
Zyrakia <ole.lammers@pm.me>
2025-10-29 22:11:56 -0400

    partial(feat): login form design and error message

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,58 +1,86 @@
-<script>
+<script lang="ts">
 	import InputGroup from '$lib/components/input-group.svelte';
 	import { login } from '$lib/remotes/auth.remote.js';
 	import { useHasJs } from '$lib/runes/has-js.svelte.js';
 	import { CredentialsSchema } from '$lib/schemas/auth.js';
 	import { EyeClosedIcon, EyeIcon } from '@lucide/svelte';
+	import { fade } from 'svelte/transition';
 
-	const remote = login.preflight(CredentialsSchema.omit({ username: true }));
 	const hasJs = useHasJs();
 
+	const getIssue = () => remote.fields.issues()?.[0].message;
+
+	const remote = login.preflight(CredentialsSchema.omit({ username: true }));
 	let showPassword = $state(false);
+
+	let issue = $state<string | undefined>(getIssue());
+	let issueClearTimeout: NodeJS.Timeout | undefined;
+	$effect(() => {
+		issue;
+
+		clearTimeout(issueClearTimeout);
+		issueClearTimeout = setTimeout(() => {
+			issueClearTimeout = undefined;
+			issue = undefined;
+		}, 5000);
+	});
 </script>
 
-<div class="flex-1 p-3 mx-auto container flex flex-col items-center justify-center">
-	<h1 class="text-3xl">Welcome Back</h1>
-	<p class="text-md pt-1">Sign in to get started</p>
-
-	<form
-		class="container mt-3 w-full flex flex-col gap-5 rounded"
-		{...remote}
-		oninput={() => remote.validate({ preflightOnly: true })}
+<div class="flex-1 p-3 flex items-center justify-center bg-linear-to-br to-30% from-emerald-50 to-gray-300">
+	<div
+		class="mx-auto container max-w-3xl flex flex-col items-center justify-center bg-white px-12 py-16 rounded-xl shadow-xl"
 	>
-		<InputGroup label="Email" error={remote.fields.email.issues()}>
-			{#snippet control()}
-				<input {...remote.fields.email.as('text')} />
-			{/snippet}
-		</InputGroup>
+		<h1 class="text-3xl">Welcome Back</h1>
+		<p class="text-md pt-1">Sign in to get started</p>
 
-		<InputGroup label="Password" error={remote.fields.password.issues()}>
-			{#snippet control()}
-				<div class="w-full relative flex gap-2">
-					<input
-						class="w-full"
-						{...remote.fields.password.as(showPassword ? 'text' : 'password')}
-					/>
+		<p class="transition-opacity text-red-400 font-bold mt-5 self-start" class:opacity-100={!!issue}>
+			{#if issue}
+				<span in:fade={{ duration: 150 }} out:fade={{ duration: 150 }}>
+					{issue}
+				</span>
+			{:else}
+				<br />
+			{/if}
+		</p>
 
-					{#if hasJs()}
-						<button type="button" onclick={() => (showPassword = !showPassword)}>
-							{#if showPassword}
-								<EyeIcon />
-							{:else}
-								<EyeClosedIcon />
-							{/if}
-						</button>
-					{/if}
-				</div>
-			{/snippet}
-		</InputGroup>
+		<form
+			class="container mt-6 w-full flex flex-col gap-5 rounded"
+			oninput={() => remote.validate({ preflightOnly: true })}
+			{...remote}
+		>
+			<InputGroup label="Email" error={remote.fields.email.issues()}>
+				{#snippet control()}
+					<input {...remote.fields.email.as('text')} />
+				{/snippet}
+			</InputGroup>
 
-		<button class="bg-green-200" {...remote.buttonProps} disabled={!!remote.pending}>
-			Login
-		</button>
+			<InputGroup label="Password" error={remote.fields.password.issues()}>
+				{#snippet control()}
+					<div class="w-full relative flex gap-2">
+						<input
+							class="w-full"
+							{...remote.fields.password.as(showPassword ? 'text' : 'password')}
+						/>
 
-		{#if remote.fields.issues()?.length}
-			<p>{remote.fields.issues()?.[0]?.message}</p>
-		{/if}
-	</form>
+						{#if hasJs()}
+							<button
+								title="Show Password"
+								class="button"
+								type="button"
+								onclick={() => (showPassword = !showPassword)}
+							>
+								{#if showPassword}
+									<EyeIcon />
+								{:else}
+									<EyeClosedIcon />
+								{/if}
+							</button>
+						{/if}
+					</div>
+				{/snippet}
+			</InputGroup>
+
+			<button class="bg-green-200" {...remote.buttonProps} disabled={!!remote.pending}> Login </button>
+		</form>
+	</div>
 </div>
