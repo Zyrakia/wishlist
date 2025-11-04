a9e42fd5b48dd8991b9a6592bf92e6b6f5b5c05f
Zyrakia <ole.lammers@pm.me>
2025-11-04 12:25:17 -0500

    feat: wishlist editor component

--- src/lib/schemas/wishlist.ts
+++ src/lib/schemas/wishlist.ts
@@ -2,14 +2,14 @@ import z from 'zod';
 
 export const WishlistSchema = z.object({
 	name: z.string().trim().min(3, 'Minimum 3 characters').max(24, 'Maximum 24 characters'),
-	description: z.string().trim().default(''),
+	description: z.string().trim().max(255, { error: 'Maximum 255 characters' }).default(''),
 	slug: z
 		.string()
 		.trim()
 		.toLowerCase()
 		.min(3, 'Minimum 3 characters')
 		.max(24, 'Maximum 24 characters')
-		.regex(/^[A-Za-z0-9]+$/, { error: 'Only letters and numbers' }),
+		.regex(/^[\_\-\.a-zA-Z0-9]+$/, { error: 'Only letters and numbers' }),
 });
 
 export type Wishlist = z.infer<typeof WishlistSchema>;
