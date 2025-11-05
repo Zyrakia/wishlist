612329564f1522669964288faafb6aa5d81a6bd0
Zyrakia <ole.lammers@pm.me>
2025-11-05 10:11:12 -0500

    build: prepare for deployment

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,8 +1,5 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
-import Database from 'better-sqlite3';
 import * as schema from './schema';
 import ENV from '../env.server';
 
-const client = new Database(ENV.DATABASE_URL);
-
-export const db = drizzle(client, { schema, casing: 'snake_case' });
+export const db = drizzle(ENV.DATABASE_URL, { schema, casing: 'snake_case' });
