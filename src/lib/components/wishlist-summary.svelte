2c1803e59d51c366f91bb61212bf58a2a4785f20
Zyrakia <ole.lammers@pm.me>
2025-11-12 10:09:42 -0500

    feat: optional author on wishlist summary

--- src/lib/components/wishlist-summary.svelte
+++ src/lib/components/wishlist-summary.svelte
@@ -1,22 +1,35 @@
 <script lang="ts">
 	import type { Wishlist } from '$lib/schemas/wishlist';
-	import { GiftIcon } from '@lucide/svelte';
+	import { GiftIcon, UserIcon } from '@lucide/svelte';
 	import type { Snippet } from 'svelte';
 
-	let { wishlist, footer }: { wishlist: Wishlist; footer?: Snippet<[]> } = $props();
+	let {
+		wishlist,
+		author,
+		footer,
+	}: { wishlist: Wishlist; author?: string; footer?: Snippet<[]> } = $props();
 </script>
 
 <a
 	href="/lists/{wishlist.slug}"
 	class="button flex max-w-full min-w-xs flex-1 flex-col justify-center border bg-surface p-3 md:max-w-xs lg:max-w-sm"
 >
-	<div class="flex flex-1 items-center gap-2">
+	<div class="flex flex-1 flex-wrap gap-2">
 		<GiftIcon class="text-danger" />
 		<p class="text-lg font-semibold">{wishlist.name}</p>
+
+		{#if author}
+			<div class="font-base ms-auto flex gap-1 text-text-muted">
+				<UserIcon size={16} />
+				<p>{author}</p>
+			</div>
+		{/if}
 	</div>
 
 	{#if wishlist.description}
-		<p class="mt-1 font-light wrap-break-word whitespace-pre-wrap">{wishlist.description}</p>
+		<p class="mt-1 overflow-hidden font-light text-nowrap text-ellipsis">
+			{wishlist.description}
+		</p>
 	{/if}
 
 	<div class="mt-auto">
