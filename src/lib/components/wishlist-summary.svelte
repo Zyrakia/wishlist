fac481296f3f58f5726156c9a838f1ade25f6d01
Zyrakia <ole.lammers@pm.me>
2025-11-13 10:07:48 -0500

    feat: centered wishlist title when no footer or description

--- src/lib/components/wishlist-summary.svelte
+++ src/lib/components/wishlist-summary.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { Wishlist } from '$lib/schemas/wishlist';
-	import { GiftIcon, UserIcon } from '@lucide/svelte';
+	import { CircleUserRound, GiftIcon, UserIcon } from '@lucide/svelte';
 	import type { Snippet } from 'svelte';
 
 	let {
@@ -12,15 +12,15 @@
 
 <a
 	href="/lists/{wishlist.slug}"
-	class="button flex max-w-full min-w-xs flex-1 flex-col justify-center border bg-surface p-3 md:max-w-xs lg:max-w-sm"
+	class="button flex max-w-full min-w-64 flex-1 flex-col justify-center border bg-surface p-3 md:max-w-xs lg:max-w-sm"
 >
-	<div class="flex flex-1 flex-wrap gap-2">
+	<div class="flex flex-1 flex-wrap gap-2" class:items-center={!wishlist.description && !footer}>
 		<GiftIcon class="text-danger" />
 		<p class="text-lg font-semibold">{wishlist.name}</p>
 
 		{#if author}
-			<div class="font-base ms-auto flex gap-1 text-text-muted">
-				<UserIcon size={16} />
+			<div class="ms-auto flex items-center gap-2 text-text-muted">
+				<CircleUserRound size={16} />
 				<p>{author}</p>
 			</div>
 		{/if}
