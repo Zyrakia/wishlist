e8d0d577269971f53dca081f7ddca458bedf5e59
Zyrakia <ole.lammers@pm.me>
2025-11-17 17:57:09 -0500

    feat: more identifiable wishlist author

--- src/lib/components/wishlist-summary.svelte
+++ src/lib/components/wishlist-summary.svelte
@@ -8,41 +8,42 @@
 		author,
 		footer,
 	}: { wishlist: Wishlist; author?: string; footer?: Snippet<[]> } = $props();
-
-	const hasBody = $derived(!!wishlist.description || !!footer);
 </script>
 
 <a
 	href="/lists/{wishlist.slug}"
-	class="button flex w-full max-w-full flex-col justify-center border bg-surface p-3"
+	class="button flex w-full max-w-full flex-col border bg-surface p-0"
 >
-	<div class="flex flex-col justify-center gap-1">
-		{#if author}
-			<div class="flex items-center gap-2 text-sm text-text-muted">
-				<CircleUserRound size={22} class="text-accent" />
-				<p class="truncate italic">
-					From <span class="not-italic underline text-text">{author}</span>
-				</p>
-			</div>
-		{/if}
+	{#if author}
+		<div
+			class="flex w-max max-w-1/2 items-center gap-2 rounded-br border-r border-b border-border bg-accent/15 px-4 py-2"
+		>
+			<CircleUserRound size={18} class="text-accent" />
 
-		<div class="flex items-center gap-1.5">
+			<p class="truncate">{author}</p>
+		</div>
+	{/if}
+
+	<div class="flex h-full flex-col gap-2 p-3">
+		<div class="flex flex-1 items-center gap-1.5">
 			<GiftIcon class="text-success" size={24} />
 			<p class="text-base font-semibold wrap-anywhere">{wishlist.name}</p>
 		</div>
-	</div>
 
-	{#if hasBody}
 		{#if wishlist.description}
 			<p class="mt-1 truncate font-light">
 				{wishlist.description}
 			</p>
+		{:else if footer}
+			<p class="font-light text-text-muted italic">No description...</p>
 		{/if}
 
-		<div class="mt-auto">
-			{#if footer}
-				{@render footer()}
-			{/if}
-		</div>
-	{/if}
+		{#if footer}
+			<div class="mt-auto">
+				{#if footer}
+					{@render footer()}
+				{/if}
+			</div>
+		{/if}
+	</div>
 </a>
