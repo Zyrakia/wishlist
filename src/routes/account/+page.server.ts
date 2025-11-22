f77ea2241efc1e5e5ff01ed8d548511a8907b928
Zyrakia <ole.lammers@pm.me>
2025-11-22 16:23:16 -0500

    chore: reverted accidental account page changes

--- src/routes/account/+page.server.ts
+++ src/routes/account/+page.server.ts
@@ -1,18 +1,12 @@
 import { resolveMe } from '$lib/remotes/auth.remote';
-import { safePruneParams } from '$lib/util/safe-prune';
+
+import { redirect } from '@sveltejs/kit';
 
 import type { PageServerLoad } from './$types';
-import z from 'zod';
 
-const paramsSchema = z.object({
-	mode: z.enum(['changeEmail', 'emailChanged']),
-	newEmail: z.string(),
-	token: z.string(),
-});
+export const load: PageServerLoad = async () => {
+	const me = await resolveMe();
+	if (!me) redirect(303, '/login?return=/account');
 
-export const load: PageServerLoad = async (event) => {
-	const me = await resolveMe({});
-
-	const params = safePruneParams(paramsSchema, event.url.searchParams);
-	return { me, ...params };
+	return { me };
 };
--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-	import { changeEmailStart } from '$lib/remotes/auth.remote';
-	import { clock } from '$lib/runes/clock.svelte';
 	import { formatRelative } from '$lib/util/date.js';
-	import { ClockIcon, Edit2Icon, MailIcon, SquarePenIcon, UserIcon } from '@lucide/svelte';
+	import { ClockIcon, type IconProps, MailIcon, UserIcon } from '@lucide/svelte';
+	import type { Component } from 'svelte';
 
+	type IconComponent = Component<IconProps, {}, ''>;
 	let { data } = $props();
 
 	const me = $derived(data.me);
-	const mode = $derived(data.mode);
 
 	const dtf = new Intl.DateTimeFormat(navigator.languages, {
 		dateStyle: 'medium',
@@ -15,7 +14,22 @@
 	});
 
 	const createdDate = $derived(dtf.format(me.createdAt));
-	const accountAge = $derived.by(() => formatRelative(me.createdAt, clock.now));
+	const accountAge = $derived.by(() => formatRelative(me.createdAt));
+
+	const info = $derived.by(
+		(): { title: string; key: keyof typeof me; content: string; icon: IconComponent }[] => {
+			return [
+				{ title: 'Name', key: 'name', content: me.name, icon: UserIcon },
+				{ title: 'Email', key: 'email', content: me.email, icon: MailIcon },
+				{
+					title: 'Created',
+					key: 'createdAt',
+					content: `${createdDate}\n( ${accountAge} )`,
+					icon: ClockIcon,
+				},
+			];
+		},
+	);
 </script>
 
 <div class="grid h-full w-full place-items-center bg-background">
@@ -25,77 +39,26 @@
 		<hr class="mt-1 mb-4" />
 
 		<div class="mb-4 flex flex-wrap gap-4">
-			<div class="flex w-full flex-col gap-4 rounded-md border border-border p-4 shadow-md">
-				<div class="flex flex-1 justify-between">
-					<p class="font-bold">Name</p>
+			{#each info as infoCard}
+				{@const Icon = infoCard.icon}
 
-					<UserIcon />
-				</div>
-
-				<p class="flex-1 font-light wrap-break-word whitespace-pre-wrap">
-					{me.name}
-				</p>
-			</div>
-		</div>
-
-		<div class="mb-4 flex flex-wrap gap-4">
-			<div class="flex w-full flex-col gap-4 rounded-md border border-border p-4 shadow-md">
-				<div class="flex flex-1 justify-between gap-3">
-					<p class="font-bold">Email</p>
-
-					<MailIcon />
-				</div>
-
-				<p
-				class="flex flex-1 flex-wrap items-center gap-4 font-light wrap-break-word whitespace-pre-wrap"
+				<div
+					class="flex w-full flex-col gap-4 rounded-md border border-border p-4 shadow-md"
 				>
-					{#if mode !== 'changeEmail'}
+					<div class="flex flex-1 justify-between">
+						<p class="font-bold">{infoCard.title}</p>
 
-					{me.email}
-					{:else}
-					<form class="flex flex-col gap-2" {...changeEmailStart}>
-						<div class="flex gap-2">
-							<input class="flex-1" />
-							<button></button>
-						</div>
-					</form>
-					{/if}
-				
-				{#if mode === 'emailChanged'}
-				<span
-				class="bg-succes/5 rounded-lg border border-success/50 px-2 py-1.5 text-sm"
-						>
-							Recently Updated
-						</span>
-					{/if}
+						<Icon />
+					</div>
 
-					{#if mode !== 'changeEmail'}
-						<a href="/" class="button ms-auto p-1.5">
-							<SquarePenIcon class="text-accent" size={18} />
-						</a>
-					{/if}
-				</p>
-			</div>
-		</div>
-
-		<div class="mb-4 flex flex-wrap gap-4">
-			<div class="flex w-full flex-col gap-4 rounded-md border border-border p-4 shadow-md">
-				<div class="flex flex-1 justify-between">
-					<p class="font-bold">Created</p>
-
-					<ClockIcon />
+					<p class="flex-1 font-light wrap-break-word whitespace-pre-wrap">
+						{infoCard.content}
+					</p>
 				</div>
-
-				<p class="flex-1 font-light wrap-break-word whitespace-pre-wrap">
-					{createdDate}
-					<br />
-					( {accountAge} )
-				</p>
-			</div>
+			{/each}
 		</div>
 
-		<a class="button mt-auto bg-danger text-center dark:text-accent-fg" href="/logout">
-			Logout
-		</a>
+		<a class="button mt-auto bg-danger text-center dark:text-accent-fg" href="/logout">Logout</a
+		>
 	</div>
 </div>
