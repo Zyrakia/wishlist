770a24a3606081502c421087c70873ea85348b03
Zyrakia <ole.lammers@pm.me>
2025-11-24 15:47:57 -0500

    refactor: theme cookie is not secure

--- src/lib/server/cookies.ts
+++ src/lib/server/cookies.ts
@@ -5,7 +5,7 @@ import ENV from './env.server';
 import { cookieHandle } from './util/cookie-handle';
 
 export const Cookie = {
-	theme: cookieHandle('theme', { path: '/', maxAge: ms('1yr') / 1000 }),
+	theme: cookieHandle('theme', { path: '/', secure: false, maxAge: ms('1yr') / 1000 }),
 	session: cookieHandle('session', {
 		path: '/',
 		httpOnly: true,
