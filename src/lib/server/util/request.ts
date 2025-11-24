d78c01295cae01e573e1f69bd492c2e00150849d
Zyrakia <ole.lammers@pm.me>
2025-11-24 07:59:57 -0500

    fix: address header read guard

--- src/lib/server/util/request.ts
+++ src/lib/server/util/request.ts
@@ -3,7 +3,7 @@ import ENV from '../env.server';
 
 export function getRequestAddress(event: RequestEvent) {
 	const header = ENV.ADDRESS_HEADER;
-	if (header) {
+	if (header && event.request.headers.has(header)) {
 		const raw = event.request.headers.get(header);
 		if (raw) return raw.split(',')[0].trim();
 	}
