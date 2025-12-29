bca458f366cebcc6ce51e71d7bb58ca42ddbf184
Zyrakia <ole.lammers@pm.me>
2025-12-29 12:11:49 -0500

    feat: search bar keyboard shortcuts

--- src/lib/components/search.svelte
+++ src/lib/components/search.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { useHasJs } from '$lib/runes/has-js.svelte';
-	import { SearchIcon, XIcon } from '@lucide/svelte';
+	import { SearchIcon } from '@lucide/svelte';
 
 	const hasJs = useHasJs();
 
@@ -12,14 +12,35 @@
 	$effect(() => {
 		if (!dialogRef) return;
 
-		if (searching) {
-			dialogRef?.showModal();
-		} else {
-			dialogRef?.close();
-			searchRef?.blur();
-		}
+		if (searching) dialogRef?.showModal();
+		else dialogRef?.close();
 	});
+
+	$effect(() => {
+		if (!searchRef) return;
+
+		if (searching) searchRef?.focus();
+		else searchRef.blur();
+	});
+
+	const onSearchFocus = () => {
+		if (!searching) searching = true;
+	};
+
+	const onSearchBlur = () => {
+		if (searching) searching = false;
+	};
+
+	const handleKeyUp = (ev: KeyboardEvent) => {
+		if (searching) {
+			if (ev.key === 'Escape') searchRef?.blur();
+		} else {
+			if (ev.key === '/') searchRef?.focus();
+		}
+	};
 </script>
+
+<svelte:window onkeyup={handleKeyUp} />
 
 {#if hasJs()}
 	<div class="flex w-full items-center justify-center gap-4 px-4">
@@ -28,13 +49,20 @@
 			class={`shrink-0 transition-colors ${searching ? 'text-accent' : 'text-text'}`}
 		/>
 
-		<input
-			name="Searching toggle"
-			bind:this={searchRef}
-			onfocus={() => (searching = true)}
-			onblur={() => (searching = false)}
-			class="w-full max-w-full px-3 transition-all duration-300 focus:w-full md:w-sm"
-			placeholder="✨ Search people, items or ask questions..."
-		/>
+		<div
+			class="relative w-full max-w-full px-3 transition-all duration-300 {searching
+				? 'md:w-full'
+				: 'md:w-2/3'}"
+		>
+			<input
+				name="Global Search"
+				aria-haspopup="dialog"
+				bind:this={searchRef}
+				onfocus={() => onSearchFocus()}
+				onblur={() => onSearchBlur()}
+				class="w-full"
+				placeholder="✨ Search people, items or ask questions..."
+			/>
+		</div>
 	</div>
 {/if}
